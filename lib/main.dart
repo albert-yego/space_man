0534709f4bf0465bfd3851038d85806a52fdc222
RAMAZAN GENCER <73017548+rmzngencer@users.noreply.github.com>
2023-05-31 10:59:22 +0300

    version solvedsc

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your applicati
+  // This widget is the root of your appicatcsacasion.
   @override
   Widget build(BuildContext context) {
     return MaterialApp();
